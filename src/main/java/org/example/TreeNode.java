4322c09f55e3fb65bbbb51ee85ee647dca14a26f
Bai Yulong <7815111+repo-yulong@user.noreply.gitee.com>
2022-08-01 19:01:10 +0800

    二叉树遍历

---
+++ src/main/java/org/example/TreeNode.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author Bai Yulong<baiyl3@lenovo.com>
+ * @version 0.1.0
+ * @since 0.1.0
+ **/
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+}
---
+++ src/main/java/org/example/day6/D61.java
@@ -0,0 +1,30 @@
+package org.example.day6;
+
+import org.example.TreeNode;
+
+
+/**
+ * 2022-08-01
+ * 二叉树遍历
+ *
+ * @author YULONG
+ */
+public class D61 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void pre(TreeNode head) {
+
+
+    }
+
+    public static void mid(TreeNode head) {
+
+    }
+
+    public static void pos(TreeNode head) {
+
+    }
+}
