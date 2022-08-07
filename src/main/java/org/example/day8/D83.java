cfa951f79c8f4d7e37d585eda7b8cda317cf8c88
Bai Yulong <7815111+repo-yulong@user.noreply.gitee.com>
2022-08-07 21:28:21 +0800

    链表求中点

---
+++ src/main/java/org/example/day8/D83.java
@@ -0,0 +1,62 @@
+package org.example.day8;
+
+import org.example.SingleNode;
+
+
+/**
+ * 2022-08-07
+ * 链表求中点，奇数个数链表求中点，偶数个数链表求前中点
+ *
+ * @author YULONG
+ */
+public class D83 {
+
+    public static void main(String[] args) {
+        SingleNode head = new SingleNode(1);
+        head.next(2).next(3).next(4).next(5).next(6).next(7);
+        SingleNode mid = f(head);
+        System.out.println(mid.value);
+    }
+
+    /**
+     * 奇数时返回中点，偶数时返回前中点
+     *
+     * @param head 1
+     * @return org.example.SingleNode
+     * @author Bai Yulong
+     * @since 0.1.0
+     */
+    public static SingleNode f(SingleNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        SingleNode slow = head.next;
+        SingleNode fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 奇数时返回中点，偶数时返回后中点
+     *
+     * @param head 1
+     * @return org.example.SingleNode
+     * @author Bai Yulong
+     * @since 0.1.0
+     */
+    public static SingleNode f1(SingleNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        SingleNode slow = head.next;
+        SingleNode fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow.next;
+    }
+}
